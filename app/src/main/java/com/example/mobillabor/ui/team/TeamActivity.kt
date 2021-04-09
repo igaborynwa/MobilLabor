f15d4027ee522c3002b5b7673d799258d08cd285
Imre Gábor <igaborynwa@gmail.com>
2021-04-09 10:16:51 +0200

    Ui package kialakítása

---
+++ app/src/main/java/com/example/mobillabor/ui/team/TeamActivity.kt
@@ -0,0 +1,12 @@
+package com.example.mobillabor.ui.team
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+import com.example.mobillabor.R
+
+class TeamActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_team)
+    }
+}
